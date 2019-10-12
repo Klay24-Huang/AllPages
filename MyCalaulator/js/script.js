3f2271a73f17adbb6bdb8e83e82da972982f0808
SeanCode2019 <wkus963@gmail.com>
2019-10-12 17:04:24 +0800

    備份

--- MyCalaulator/js/script.js
+++ MyCalaulator/js/script.js
@@ -4,29 +4,36 @@ $(function () {
 });
 
 var input = '';
-var result;
+
 
 function Click() {
-    $('td').click(function (e) { 
-        // if ($(this).val() == '=') return;
-        input += $(this).text();
-        console.log(input);
-        $('input').val(input);
+    $('td').click(function (e) {
+        if (($(this).text()) == '=') {
+            return;
+        } else if (($(this).text()) == 'ac') {
+            $('input').val('0');
+            input ='';
+            // input = '0';
+            // $('input').val(input);
+        } else {
+            input += $(this).text();
+            $('input').val(input);
+
+            
+        }
     });
 }
 
-function Calculate () {
-$('#calculate').click(function (e) { 
-    input -= '=';
-    result = parseInt(input);
-    $('input').val(results)
-});
-    
+function Calculate() {
+    $('#calculate').click(function (e) {
+        $('input').val(eval(input));
+    });
+
 }
 
-function Show() {
-    $('input').val(input);
-}
+// function Show() {
+//     $('input').val(input);
+// }
 
 
 
