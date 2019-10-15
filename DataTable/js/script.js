35b788c859902d4af183bcbc31e88d0dcc979217
SeanCode2019 <wkus963@gmail.com>
2019-10-16 00:49:54 +0800

    通訊錄 初版

--- DataTable/js/script.js
+++ DataTable/js/script.js
@@ -22,9 +22,14 @@ $(function () {
     });
     //匯入資料
     $('#ButtonImport').click(Import);
-
     //新增資料
     $('#btn-modal-add').click(ModalAddData);
+    // 修改資料
+    $('#btn-modified').click(ButtonUpdate);
+    // 刪除資料
+    $('#btn-delete').click(ButtonDelete);
+    //參考: https://datatables.net/examples/advanced_init/events_live.html
+    $('#table_id tbody').on('click', 'tr', ButtonModified);
 });
 var isImport; //匯入匯出flag
 var dat = [
@@ -95,6 +100,7 @@ function ImportOrExport() {
     $('#ModalDataExport').modal();
 }
 
+// 匯入
 function Import() {
     var str = $('#field_DataJSON').val();
     dat = JSON.parse(str);
@@ -102,7 +108,52 @@ function Import() {
     RenewModal();
 }
 
+// 匯出
 function Export() {
     var exportData = JSON.stringify(dat);
     $('#field_DataJSON').val(exportData);
+}
+
+//刪除資料
+function ButtonDelete() {
+    //找出當前編輯的資料是哪一個元素
+    var n = dat.indexOf(CurrentEditItem);
+    //刪除指定位置的元素
+    dat.splice(n, 1);
+    //refresh data table
+    RenewModal();
+    //儲存記憶體中的資料到localstorage
+    SaveData();
+}
+
+//更新資料
+function ButtonUpdate() {
+    //取得當前編輯資料
+    var NewItem = [
+        $('#EditField_Name').val(),
+        $('#EditField_Birthday').val(),
+        $('#EditField_TEL').val(),
+        $('#EditField_Email').val(),
+        $('#EditField_Address').val()
+    ];
+    //找出當前編輯的資料是哪一個元素
+    var n = dat.indexOf(CurrentEditItem);
+    //置換
+    dat[n] = NewItem;
+    //refresh data table
+    RenewModal();
+    //儲存記憶體中的資料到localstorage
+    SaveData();
+}
+
+function ButtonModified() {
+    //取得當前點選的資料 
+    CurrentEditItem = dt.row(this).data();
+    $('#EditField_Name').val(CurrentEditItem[0]);
+    $('#EditField_Birthday').val(CurrentEditItem[1]);
+    $('#EditField_TEL').val(CurrentEditItem[2]);
+    $('#EditField_Email').val(CurrentEditItem[3]);
+    $('#EditField_Address').val(CurrentEditItem[4]);
+    //顯示當前點選的資料
+    $('#ModifiedModal').modal();
 }
